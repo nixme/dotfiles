218967a73d7c9e9505990df69d3c7bb5ecbf61d7
Gopal Patel <nixme@stillhope.com>
2011-01-09 05:18:35 +0530

    Bind M-s to prepend sudo to current line.
    Also move bindings.zsh into editing.zsh.

--- .zsh/config/bindings.zsh
+++
@@ -1,8 +0,0 @@
-bindkey -e   # generally use emacs keybindings
-
-# search on up/down arrow if a partial cmd is entered, otherwise browse history
-bindkey '^[[A' up-line-or-search
-bindkey '^[[B' down-line-or-search
-
-# expand history abbreviations when hitting space
-bindkey ' ' magic-space
--- .zsh/config/editing.zsh
+++ .zsh/config/editing.zsh
@@ -1,3 +1,21 @@
+bindkey -e   # generally use emacs keybindings
+
 # remove '/' from default set of characters that are treated as part of a word,
 #   makes backward-delete-word and others more useful with paths
 WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
+
+# search on up/down arrow if a partial cmd is entered, otherwise browse history
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+
+# expand history abbreviations when hitting space
+bindkey ' ' magic-space
+
+# bind M-s to prepend 'sudo ' to the current line
+# TODO: detect solaris and use 'pfexec' instead of 'sudo'
+insert-sudo () {
+  BUFFER="sudo $BUFFER"
+  CURSOR=$#BUFFER
+}
+zle -N insert-sudo
+bindkey '^[s' insert-sudo
