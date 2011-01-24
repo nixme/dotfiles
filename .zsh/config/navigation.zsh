a493b285a89d453359d5df69a1c839d83b7fb12d
Gopal Patel <nixme@stillhope.com>
2011-01-24 11:02:16 -0800

    zsh: List directory contents on directory change.

--- .zsh/config/navigation.zsh
+++ .zsh/config/navigation.zsh
@@ -3,3 +3,7 @@ setopt auto_pushd         # automatically maintain directory stack
 setopt pushd_ignore_dups  # ignore dups in directory stack
 
 DIRSTACKSIZE=20           # max size of directory stack
+
+# list directory whenever working directory changes
+chpwd_ls() { l; }
+chpwd_functions+=chpwd_ls
